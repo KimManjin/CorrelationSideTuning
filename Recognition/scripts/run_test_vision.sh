8091ae8777fb1667a32722de15e2975237ec2057
KimManjin <mandos@postech.ac.kr>
2024-08-28 16:19:29 +0900

    refactor scripts

--- Recognition/scripts/run_test_vision.sh
+++ Recognition/scripts/run_test_vision.sh
@@ -1,11 +1,46 @@
 #!/usr/bin/env bash
-if [ -f $1 ]; then
-  config=$1
+if [ $1 -eq 0 ]; then
+    gpus=0,1,2,3,4,5,6,7
+    n_gpus=8
+elif [ $1 -eq 1 ]; then
+    gpus=0,1,2,3
+    n_gpus=4
+elif [ $1 -eq 2 ]; then
+    gpus=4,5,6,7
+    n_gpus=4
+else
+    echo "Invalid GPU setting"
+    exit 1
+fi
+
+# num_threads=48
+# num_workers=4
+
+if [ -f $2 ]; then
+  config=$2
 else
   echo "need a config file"
   exit
 fi
 
-weight=$2
-python -m torch.distributed.launch --master_port 1438 --nproc_per_node=8 \
-    test_vision.py --config ${config} --weights ${weight} ${@:3}
+weight=$3
+
+# Automatic port setting.
+port=1438
+while true; do
+  # Check if the port is in use using netstat
+  if netstat -tuln | awk '{print $4}' | grep -E "0.0.0.0:$port|:::$port" > /dev/null; then
+    echo "Port $port is in use. Trying the next port."
+    port=$((port + 1))
+  else
+    echo "Port $port is available."
+    break
+  fi
+done
+
+# MKL_NUM_THREADS=${num_threads} \
+# NUMEXPR_NUM_THREADS=${num_threads} \
+# OMP_NUM_THREADS=${num_threads} \
+CUDA_VISIBLE_DEVICES=${gpus} \
+python -m torch.distributed.launch --master_port=${port} --nproc_per_node=${n_gpus} \
+          test_vision.py --config ${config} --weights ${weight} ${@:4}
--- Recognition/scripts/run_train_vision.sh
+++ Recognition/scripts/run_train_vision.sh
@@ -1,12 +1,47 @@
 #!/usr/bin/env bash
-if [ -f $1 ]; then
-  config=$1
+if [ $1 -eq 0 ]; then
+    gpus=0,1,2,3,4,5,6,7
+    n_gpus=8
+elif [ $1 -eq 1 ]; then
+    gpus=0,1,2,3
+    n_gpus=4
+elif [ $1 -eq 2 ]; then
+    gpus=4,5,6,7
+    n_gpus=4
+else
+    echo "Invalid GPU setting"
+    exit 1
+fi
+
+# num_threads=48
+# num_workers=4
+
+if [ -f $2 ]; then
+  config=$2
 else
   echo "need a config file"
   exit
 fi
 
-now=$(date +"%Y%m%d_%H%M%S")
-python -m torch.distributed.run --master_port 2334 --nproc_per_node=8 \
-         train_vision.py  --config ${config} --log_time $now 
+exp_name=$3
+
+# Automatic port setting.
+port=2335
+while true; do
+  # Check if the port is in use using netstat
+  if netstat -tuln | awk '{print $4}' | grep -E "0.0.0.0:$port|:::$port" > /dev/null; then
+    echo "Port $port is in use. Trying the next port."
+    port=$((port + 1))
+  else
+    echo "Port $port is available."
+    break
+  fi
+done
+
+# MKL_NUM_THREADS=${num_threads} \
+# NUMEXPR_NUM_THREADS=${num_threads} \
+# OMP_NUM_THREADS=${num_threads} \
+CUDA_VISIBLE_DEVICES=${gpus} \
+python -m torch.distributed.run --master_port=${port} --nproc_per_node=${n_gpus} \
+         train_vision.py  --config ${config} --exp_name ${exp_name}
 
